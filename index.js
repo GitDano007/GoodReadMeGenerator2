201611d1bd9c51216ffc9139e56f359fb1be2ac2
Dano Seese <dpseese@yahoo.com>
2020-09-05 15:54:19 -0700

    updates

---
+++ index.js
@@ -0,0 +1,72 @@
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your projects:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide any installation instructions for your project:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide any usage information:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license you would like to include:",
+        choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please describe any contributions that were made:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please describe any tests for your project:"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
+];
+
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+};
+
+
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const data = generateMarkdown(answers);
+        writeToFile("README.md", data);
+        console.log("Successfully wrote to file!");
+    });
+};
+
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,36 @@
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Licenses](#Licenses)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Licenses
+This application is covered under ${data.license}.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any further questions, feel free to contact me on GitHub at http://github.com/${data.github} or send me an email at: ${data.email}.
+`;
+}
+
+module.exports = generateMarkdown;
